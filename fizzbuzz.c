6d052ea5b22c2a135f1a77ed51e1b8264194aabd
Yasirul Choudhury <yasirgchoudhury@utexas.edu>
2018-04-01 18:32:57 -0500

    Added documentation

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,8 +1,8 @@
-#Consider the following problem:
-#Write a short program that prints each number from 1 to 100 on a new line 
-#For each multiple of 3, print "Fizz" instead of the number
-#For each multiple of 5, print "Buzz" instead of the number
-#For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number
+//Consider the following problem:
+//Write a short program that prints each number from 1 to 100 on a new line 
+//For each multiple of 3, print "Fizz" instead of the number
+//For each multiple of 5, print "Buzz" instead of the number
+//For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number
 
 #include <stdio.h>
 #include <stdint.h>
